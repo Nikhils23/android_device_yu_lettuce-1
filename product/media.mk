ea4549ef18ce7a8f015f119dcf7e336f4aa44be6
drohan898 <drohan940@gmail.com>
2017-12-20 09:08:54 +0000

    lettuce: Move audio libraries to /vendor
    Signed-off-by: drohan898 <drohan940@gmail.com>

--- product/media.mk
+++ product/media.mk
@@ -1,3 +1,3 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/vendor/etc/media_profiles.xml
